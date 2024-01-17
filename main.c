df53d601618ae94dcb96856281fbedd75ca8554c
David Farr <davidthfarr@gmail.com>
2024-01-17 08:19:25 -0500

    main.c changes

---
+++ main.c
@@ -0,0 +1,32 @@
+/*Entry  point where program execution begins. Handles user input and interacts with other modules. */
+#include "data_structures.h"
+#include <stdio.h>
+
+int main(){
+        // Create a hash table
+        HashTable* hashTable = createHashTable(100); // Adjust size as need
+        
+        // Create a sample part
+        Part samplePart = {1001};
+        
+        // Insert the sample part into the hash table 
+        insertPart(hashTable, &samplePart);
+        
+        // Search for the part in the hash table
+        Part* foundPart = searchPart(hashTable, 1001);
+        
+        // Check if part was found and print its information
+        if (foundPart != NULL){
+            printf("Part found: Part ID - %d\n", foundPart->partID);
+        } else {
+                printf("Part not found.\n");
+        }
+        
+        // Delete the part from the hash table
+        deletePart(hashTable, 1001);
+        
+        // Free the memory allocated for the hash table
+        freeHashTable(hashTable);
+        
+        return 0;   
+}
